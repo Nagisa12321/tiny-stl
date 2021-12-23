e89bc65c28be0243cfff50343ae119cfa8d1e7fb
jt <1216414009@qq.com>
2021-12-23 21:30:37 +0800

    feat: forward_list's member function and test it

--- include/tinystl_forward_list.h
+++ include/tinystl_forward_list.h
@@ -5,6 +5,9 @@
 #include "tinystl_iterator_base.h"
 #include "tinystl_types.h"
 #include "tinystl_alloc.h"
+
+// use std iniializer_list 
+#include <initializer_list>
 namespace tinystd {
 
 struct __slist_node_base 
@@ -60,7 +63,7 @@ struct __slist_iterator : public __slist_iterator_base {
     __slist_iterator() : __slist_iterator_base(0) {}
     __slist_iterator(const __slist_iterator &__it) : __slist_iterator_base(__it._M_node) {}
 
-    reference operator*() const { return ((__slist_node<_Tp>)_M_node)._M_data; }
+    reference operator*() const { return ((__slist_node<_Tp> *)_M_node)->_M_data; }
     pointer operator->() const { return &(operator*()); }
 
     __self &operator++() { _M_incr(); return *this; }
@@ -81,12 +84,39 @@ public:
     typedef ptrdiff_t different_type;
 
     forward_list() { _M_head._M_next = nullptr; }
+    forward_list(const std::initializer_list<_Tp> &__li) : forward_list() 
+        { _M_copy_initialize(__li.begin(), __li.end()); }
+    template <typename _InputIter, tinystd::void_t<decltype(*tinystd::__declval<_InputIter>())>* = nullptr>
+    forward_list(_InputIter __lhs, _InputIter __rhs) : forward_list() 
+        { _M_copy_initialize(__lhs, __rhs); }
+    forward_list(size_type __n, const _Tp &__data = _Tp()) : forward_list() 
+        { _M_fill_initialize(__n, __data); }
+    forward_list(const forward_list<_Tp> &__li) : forward_list() 
+        { _M_copy_initialize(__li.begin(), __li.end()); }
+    
     ~forward_list() { clear(); }
 
+    forward_list &operator=(const forward_list &__li) {
+        clear();
+        _M_copy_initialize(__li.begin(), __li.end());
+        return *this;
+    }
+
+    forward_list &operator=(forward_list &&__li) {
+        _M_head._M_next = __li._M_head._M_next;
+        __li._M_head._M_next = nullptr;
+        return *this;
+    }
+
+    iterator before_begin() { return iterator((__list_node *)_M_head); }
+    const_iterator before_begin() const { return const_iterator((__list_node *)_M_head); }
     iterator begin() { return iterator((__list_node *)_M_head._M_next); }
+    const_iterator begin() const { return const_iterator((__list_node *)_M_head._M_next); }
     iterator end() { return iterator(nullptr); }
-    size_t size() const { return __slist_size(_M_head._M_next); }
+    const_iterator end() const { return const_iterator(nullptr); }
+    size_type size() const { return __slist_size(_M_head._M_next); }
     bool empty() const { return !_M_head._M_next; }
+    size_type max_size() const { return size_type(-1); }
     reference front() { return ((__list_node *) _M_head._M_next)->_M_data; }
     const_reference front() const { return ((__list_node *) _M_head._M_next)->_M_data; }
     void push_front(const _Tp &__data) { __slist_make_link(&_M_head, _M_create_node(__data)); }
@@ -100,10 +130,82 @@ public:
         __other._M_head._M_next = _M_head._M_next;
         _M_head._M_next = __tmp;
     }
-    
     void clear() {
-        while (!empty()) { pop_front(); }
+        while (!empty()) 
+            { pop_front(); }
     }
+    void assign(size_type __count, const _Tp &__value) {
+        clear();
+        _M_fill_initialize(__count, __value);
+    }
+    template <class _InputIter, tinystd::void_t<decltype(*tinystd::__declval<_InputIter>())>* = nullptr>
+    void assign(_InputIter __first, _InputIter __last) {
+        clear();
+        _M_copy_initialize(__first, __last);
+    }
+    void assign(std::initializer_list<_Tp> __ilist) {
+        clear();
+        _M_copy_initialize(__ilist.begin(), __ilist.end());
+    }
+    /**
+     * @brief 
+     * 
+     * @param __pos 
+     * @param __data 
+     * @return iterator 
+     * 1) Iterator to the inserted element.
+     * 2) Iterator to the last element inserted, or pos if count==0.
+     * 3) Iterator to the last element inserted, or pos if first==last.
+     * 4) Iterator to the last element inserted, or pos if ilist is empty.
+     */
+    iterator insert_after(iterator __pos, const _Tp &__data) {
+        __slist_make_link(__pos._M_node, _M_create_node(__data));
+        return ++__pos;
+    }
+    iterator insert_after(iterator __pos, size_type __n, const _Tp &__data) {
+        if (!__n) return __pos;
+        __list_node *__cur = (__list_node *) __pos._M_node;
+        __list_node *__tmp = (__list_node *) __cur->_M_next;
+        for (; __n > 0; --__n) {
+            __slist_make_link(__cur, _M_create_node(__data));
+            __cur = (__list_node *) __cur->_M_next;
+        }
+        __cur->_M_next = __tmp;
+        return iterator(__cur);
+    }
+    template <typename _InputIter, tinystd::void_t<decltype(*tinystd::__declval<_InputIter>())>* = nullptr>
+    iterator insert_after(iterator __pos, _InputIter __lhs, _InputIter __rhs) {
+        if (__lhs == __rhs) return __pos;
+        __list_node *__cur = (__list_node *) __pos._M_node;
+        __list_node *__tmp = (__list_node *) __cur->_M_next;
+        while (__lhs != __rhs) {
+            __slist_make_link(__cur, _M_create_node(*__lhs++));
+            __cur = (__list_node *) __cur->_M_next;
+        }
+        __cur->_M_next = __tmp;
+        return iterator(__cur);
+    }
+    iterator insert_after(iterator __pos, const std::initializer_list<_Tp> &__ili) {
+        return insert_after(__pos, __ili.begin(), __ili.end());
+    }
+    /**
+     * @brief 
+     * 
+     * @return iterator 
+     * Iterator to the element following the erased one,
+     * or end() if no such element exists.
+     */
+    iterator erase_after(iterator __pos) {
+        __list_node *__tmp = (__list_node *) __pos._M_node->_M_next;
+        __pos._M_node->_M_next = __pos._M_node->_M_next->_M_next;
+        _M_destory_node(__tmp);
+    }
+
+    iterator erase_after(iterator __lhs, iterator __rhs) {
+        
+    }
+
+
 protected:
     typedef __slist_node_base __list_node_base;
     typedef __slist_node<_Tp> __list_node;
@@ -114,13 +216,29 @@ protected:
 
     __list_node *_M_create_node(const _Tp &__data) {
         __list_node *__res = __node_allocator::_S_allocate();
-        __res->_M_data = __data;
+        // !!!Use constuctor here~
+        tinystd::construct(&__res->_M_data, __data);
         return __res;
     }
 
     void _M_destory_node(__list_node *__node) {
         tinystd::destory(&__node->_M_data);
         __node_allocator::_S_deallocate(__node);
+    }
+
+    template <typename _InputIter>
+    void _M_copy_initialize(_InputIter __lhs, _InputIter __rhs) {
+        __list_node *__cur = (__list_node *) (&_M_head);
+        while (__lhs != __rhs) {
+            __slist_make_link(__cur, _M_create_node(*__lhs++));
+            __cur = (__list_node *) __cur->_M_next;
+        }
+    }
+
+    void _M_fill_initialize(size_type __n, const _Tp &__data) {
+        for (; __n > 0; --__n) {
+            push_front(__data);
+        }
     }
 };
 
--- test/test_forward_list.cpp
+++ test/test_forward_list.cpp
@@ -1,22 +1,46 @@
+#include "tinystl_algo.h"
 #include "tinystl_forward_list.h"
 #include <iostream>
 #include <ostream>
 #include <string>
 #include <vector>
 #include <forward_list>
+#include <numeric>
 
 // template init
 template class tinystd::forward_list<int>;
 
 void __test_std_forward_list();
+void __test_normal_use();
+void __test_constructor();
+void __test_operator_eq();
+void __test_assign();
+void __test_front();
+void __test_begin();
+void __test_empty();
+void __test_maxsize();
+void __test_clear();
+void __test_insert_after();
+void __test_erase_after();
 
 int main() {
     std::vector<std::pair<std::string, void (*)()>> __test_cases{
         { "test STL's forward_list", __test_std_forward_list },
+        { "test normal use", __test_normal_use },
+        { "test constructor", __test_constructor },
+        { "test operator equal. ", __test_operator_eq },
+        { "test assign. ", __test_assign },
+        { "test front. ", __test_front },
+        { "test begin. ", __test_begin },
+        { "test empty. ", __test_empty },
+        { "test max_szie. ", __test_maxsize },
+        { "test clear. ", __test_clear },
+        { "test insert_after. ", __test_insert_after },
+        { "test erase_after. ", __test_erase_after },
     };
 
     for (const std::pair<std::string, void (*)()> &__p : __test_cases) {
-        std::cout << "\n>> " << __p.first << " ========================" << std::endl;
+        std::cout << "\n>> " << __p.first << " ========================" << std::endl;  
         __p.second();
     }
 }
@@ -44,37 +68,6 @@ std::ostream &operator<<(std::ostream &s, const tinystd::forward_list<T> &v) {
     return s << ']';
 }
 
-
-template <typename _ForwardIntList, typename _ForwardStringList>
-void __generic_test_case() {
-    std::cout << "test1: Normal use"<< std::endl;
-    {
-        _ForwardIntList mylist = {34, 77, 16, 2};
-        std::cout << "mylist contains:";
-        for (auto it = mylist.begin(); it != mylist.end(); ++it)
-            std::cout << ' ' << *it;
-        std::cout << '\n';
-    }
-    std::cout << "test2: constructor: " << std::endl;
-    {
-        // c++11 initializer list syntax:
-        _ForwardStringList words1{"the", "frogurt", "is", "also", "cursed"};
-        std::cout << "words1: " << words1 << '\n';
-
-        // words2 == words1
-        _ForwardStringList words2(words1.begin(), words1.end());
-        std::cout << "words2: " << words2 << '\n';
-
-        // words3 == words1
-        _ForwardStringList words3(words1);
-        std::cout << "words3: " << words3 << '\n';
-
-        // words4 is {"Mo", "Mo", "Mo", "Mo", "Mo"}
-        _ForwardStringList words4(5, "Mo");
-        std::cout << "words4: " << words4 << '\n';
-    }
-}
-
 struct __obj {
     __obj() : _M_pi(new int) {}
     __obj(int i) : _M_pi(new int(i)) {}
@@ -90,4 +83,286 @@ struct __obj {
     int *_M_pi;
 };
 
-void __test_std_forward_list() { __generic_test_case<std::forward_list<int>, std::forward_list<std::string>>(); }
+void __test_std_forward_list() {
+    std::cout << "test1: Normal use" << std::endl;
+    {
+        std::forward_list<int> mylist = {34, 77, 16, 2};
+        std::cout << "mylist contains:";
+        for (auto it = mylist.begin(); it != mylist.end(); ++it)
+            std::cout << ' ' << *it;
+        std::cout << '\n';
+    }
+    std::cout << "test2: constructor: " << std::endl;
+    {
+        // c++11 initializer list syntax:
+        std::forward_list<std::string> words1{"the", "frogurt", "is", "also", "cursed"};
+        std::cout << "words1: " << words1 << '\n';
+
+        // words2 == words1
+        std::forward_list<std::string> words2(words1.begin(), words1.end());
+        std::cout << "words2: " << words2 << '\n';
+
+        // words3 == words1
+        std::forward_list<std::string> words3(words1);
+        std::cout << "words3: " << words3 << '\n';
+
+        // words4 is {"Mo", "Mo", "Mo", "Mo", "Mo"}
+        std::forward_list<std::string> words4(5, "Mo");
+        std::cout << "words4: " << words4 << '\n';
+    }
+}
+void __test_normal_use() {
+    std::cout << "test1: Normal use" << std::endl;
+    {
+        tinystd::forward_list<int> mylist = {34, 77, 16, 2};
+        std::cout << "mylist contains:";
+        for (auto it = mylist.begin(); it != mylist.end(); ++it)
+            std::cout << ' ' << *it;
+        std::cout << '\n';
+    }
+}
+
+void __test_constructor() {
+    // c++11 initializer list syntax:
+    tinystd::forward_list<std::string> words1{"the", "frogurt", "is", "also", "cursed"};
+    std::cout << "words1: " << words1 << '\n';
+
+    // words2 == words1
+    tinystd::forward_list<std::string> words2(words1.begin(), words1.end());
+    std::cout << "words2: " << words2 << '\n';
+
+    // words3 == words1
+    tinystd::forward_list<std::string> words3(words1);
+    std::cout << "words3: " << words3 << '\n';
+
+    // words4 is {"Mo", "Mo", "Mo", "Mo", "Mo"}
+    tinystd::forward_list<std::string> words4(5, "Mo");
+    std::cout << "words4: " << words4 << '\n';
+}
+
+void display_sizes(const tinystd::forward_list<int> &nums1,
+                   const tinystd::forward_list<int> &nums2,
+                   const tinystd::forward_list<int> &nums3) {
+    std::cout << "nums1: " << tinystd::distance(nums1.begin(), nums1.end())
+              << " nums2: " << tinystd::distance(nums2.begin(), nums2.end())
+              << " nums3: " << tinystd::distance(nums3.begin(), nums3.end()) << '\n';
+}
+
+/**
+ * @brief 
+ * Initially:
+ * nums1: 6 nums2: 0 nums3: 0
+ * After assigment:
+ * nums1: 6 nums2: 6 nums3: 0
+ * After move assigment:
+ * nums1: 0 nums2: 6 nums3: 6
+ * 
+ */
+void __test_operator_eq() {
+    tinystd::forward_list<int> nums1{3, 1, 4, 6, 5, 9};
+    tinystd::forward_list<int> nums2;
+    tinystd::forward_list<int> nums3;
+
+    std::cout << "Initially:\n";
+    display_sizes(nums1, nums2, nums3);
+
+    // copy assignment copies data from nums1 to nums2
+    nums2 = nums1;
+
+    std::cout << "After assigment:\n";
+    display_sizes(nums1, nums2, nums3);
+
+    // move assignment moves data from nums1 to nums3,
+    // modifying both nums1 and nums3
+    nums3 = std::move(nums1);
+
+    std::cout << "After move assigment:\n";
+    display_sizes(nums1, nums2, nums3);
+}
+
+/**
+ * @brief 
+ * a a a a a 
+ * b b b b b b 
+ * C + + 1 1
+ * 
+ */
+void __test_assign() {
+    tinystd::forward_list<char> characters;
+ 
+    auto print_forward_list = [&](){
+        for (char c : characters)
+            std::cout << c << ' ';
+        std::cout << '\n';  
+    };
+ 
+    characters.assign(5, 'a');
+    print_forward_list();
+ 
+    const std::string extra(6, 'b');
+    characters.assign(extra.begin(), extra.end());
+    print_forward_list();
+ 
+    characters.assign({'C', '+', '+', '1', '1'});
+    print_forward_list();
+}
+
+/**
+ * @brief 
+ * The first character is 'o'.
+ */
+void __test_front() {
+    tinystd::forward_list<char> letters {'o', 'm', 'g', 'w', 't', 'f'};
+ 
+    if (!letters.empty()) {
+        std::cout << "The first character is '" << letters.front() << "'.\n";
+    }  
+}
+
+/**
+ * @brief 
+ * 1 2 4 8 16
+ * Sum of nums: 31
+ * First fruit: orange
+ * forward_list 'empty' is indeed empty.
+ */
+void __test_begin() {
+    tinystd::forward_list<int> nums {1, 2, 4, 8, 16};
+    tinystd::forward_list<std::string> fruits {"orange", "apple", "raspberry"};
+    tinystd::forward_list<char> empty;
+ 
+    // Print forward_list.
+    tinystd::for_each(nums.begin(), nums.end(), [](const int n) { std::cout << n << ' '; });
+    std::cout << '\n';
+ 
+    // Sums all integers in the forward_list nums (if any), printing only the result.
+    std::cout << "Sum of nums: "
+              << std::accumulate(nums.begin(), nums.end(), 0) << '\n';
+
+    // Prints the first fruit in the forward_list fruits, checking if there is any.
+    if (!fruits.empty())
+        std::cout << "First fruit: " << *fruits.begin() << '\n';
+ 
+    if (empty.begin() == empty.end())
+        std::cout << "forward_list 'empty' is indeed empty.\n";
+}
+
+/**
+ * @brief 
+ * Initially, numbers.empty(): true
+ * After adding elements, numbers.empty(): false
+ */
+void __test_empty() {
+    tinystd::forward_list<int> numbers;
+    std::cout << std::boolalpha;
+    std::cout << "Initially, numbers.empty(): " << numbers.empty() << '\n';
+ 
+    numbers.push_front(42);
+    numbers.push_front(13317); 
+    std::cout << "After adding elements, numbers.empty(): " << numbers.empty() << '\n';
+}
+
+
+const char *separate(unsigned long long n) {
+    static char buf[64];
+
+    int i{sizeof(buf) - 1}, j{};
+    buf[i] = '\0';
+
+    do {
+        buf[--i] = '0' + (n % 10);
+        if (j++ % 3 == 2)
+            buf[--i] = '\'';
+    } while (n /= 10);
+
+    return buf + i + (buf[i] == '\'' ? 1 : 0);
+}
+
+/**
+ * @brief 
+ * Maximum size of a 'forward_list' is 1'152'921'504'606'846'975
+ */
+void __test_maxsize() {
+    tinystd::forward_list<char> s;
+    std::cout << "Maximum size of a 'forward_list' is " << separate(s.max_size()) << "\n";
+}
+
+/**
+ * @brief 
+ * Before clear: 1 2 3
+ * Clear
+ * After clear:
+ */
+void __test_clear() {
+    tinystd::forward_list<int> container{1, 2, 3};
+
+    auto print = [](const int &n) { std::cout << " " << n; };
+
+    std::cout << "Before clear:";
+    tinystd::for_each(container.begin(), container.end(), print);
+    std::cout << '\n';
+
+    std::cout << "Clear\n";
+    container.clear();
+
+    std::cout << "After clear:";
+    tinystd::for_each(container.begin(), container.end(), print);
+    std::cout << '\n';
+}
+
+/**
+ * @brief 
+ * words: [the, frogurt, is, also, cursed]
+ * words: [the, strawberry, frogurt, is, also, cursed]
+ * words: [the, strawberry, strawberry, strawberry, frogurt, is, also, cursed]
+ * words: [the, strawberry, strawberry, strawberry, apple, banana, cherry, frogurt, is, also, cursed]
+ * words: [the, strawberry, strawberry, strawberry, apple, banana, cherry, jackfruit, kiwifruit, lime, mango, frogurt, is, also, cursed]
+ */
+void __test_insert_after() {
+    tinystd::forward_list<std::string> words {"the", "frogurt", "is", "also", "cursed"};
+    std::cout << "words: " << words << '\n';                                    
+ 
+    // insert_after (2)                                                         
+    auto beginIt = words.begin();                                               
+    words.insert_after(beginIt, "strawberry");                                  
+    std::cout << "words: " << words << '\n';                                    
+ 
+    // insert_after (3)                                                         
+    auto anotherIt = beginIt;                                                   
+    ++anotherIt;                                                                
+    anotherIt = words.insert_after(anotherIt, 2, "strawberry");                 
+    std::cout << "words: " << words << '\n';                                    
+ 
+    // insert_after (4)
+    std::vector<std::string> V = { "apple", "banana", "cherry"};                
+    anotherIt = words.insert_after(anotherIt, V.begin(), V.end());              
+    std::cout << "words: " << words << '\n';                                    
+ 
+    // insert_after (5)                                                         
+    words.insert_after(anotherIt, {"jackfruit", "kiwifruit", "lime", "mango"});
+    std::cout << "words: " << words << '\n';   
+}
+
+/**
+ * @brief 
+ * 2 3 4 5 6 7 8 9
+ * 2 3 6 7 8 9
+ */
+void __test_erase_after() {
+    tinystd::forward_list<int> l = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    //    l.erase( l.begin() ); // ERROR: No function erase
+
+    l.erase_after(l.before_begin()); // Removes first element
+
+    for (auto n : l) std::cout << n << " ";
+    std::cout << '\n';
+
+    auto fi = std::next(l.begin());
+    auto la = std::next(fi, 3);
+
+    l.erase_after(fi, la);
+
+    for (auto n : l) std::cout << n << " ";
+    std::cout << '\n';
+}
